d06cb4959328a86ff6b14cb9b6384c5cf4677aa5
Agung Firmansyah <agungfirid@gmail.com>
2021-10-27 22:45:27 +0700

    delegate rule stylesheet

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css';
 
 let drawer = document.querySelector('#drawer nav');
 let menuDrawer = document.querySelector('#menu-drawer');
